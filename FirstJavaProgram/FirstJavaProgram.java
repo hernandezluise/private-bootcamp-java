5980df05d3de08a5c6c2a20a5ad5ef760d1e22c7
Luis Hernandez <120614329+hernandezluise@users.noreply.github.com>
2023-04-04 18:13:22 -0700

    Add files via upload

---
+++ FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Luis");
+		System.out.println("I am 26 years old");
+		System.out.println("My hometown is Seattle, Wa");
+	}
+}
