2fab290c7375aeba5c7c43d857c604feefba2f0e
Luis Hernandez <120614329+hernandezluise@users.noreply.github.com>
2023-04-19 16:34:20 -0700

    Daikichi Routes Path Variable Assignment

---
+++ DaikchiRoutes Path Variable/src/main/java/com/luishernandez/daikichiroutes/DaikchiRoutesApplication.java
@@ -0,0 +1,14 @@
+package com.luishernandez.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikchiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikchiRoutesApplication.class, args);
+	}
+
+}
