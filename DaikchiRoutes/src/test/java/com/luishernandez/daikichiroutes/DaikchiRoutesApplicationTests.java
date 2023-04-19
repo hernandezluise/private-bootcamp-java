1031fc491cffbd9e8e06dfab4416654009468adc
Luis Hernandez <120614329+hernandezluise@users.noreply.github.com>
2023-04-18 17:57:52 -0700

    Daikichi Routes assignment

---
+++ DaikchiRoutes/src/test/java/com/luishernandez/daikichiroutes/DaikchiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luishernandez.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikchiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
